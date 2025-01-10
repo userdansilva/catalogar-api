6ec046b9916d16b19b2405416d97da61d3960593
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-01-09 21:53:56 -0300

    first commit

---
+++ src/main/java/com/catalogar/CatalogarApplication.java
@@ -0,0 +1,13 @@
+package com.catalogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalogar/CatalogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
