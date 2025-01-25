0763bf8e7b34f0d393f0ec40031abf747a549a1e
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-01-25 07:44:34 -0300

    Create the User model

---
+++ src/main/java/com/catalogar/model/User.java
@@ -0,0 +1,116 @@
+package com.catalogar.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity(name = "User")
+@EntityListeners(AuditingEntityListener.class)
+@Table(
+        name = "user_profile",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "user_email_unique",
+                        columnNames = "email"
+                )
+        }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(
+            name = "id",
+            updatable = false,
+            columnDefinition = "UUID"
+    )
+    private UUID id;
+
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    @Column(
+            name = "email",
+            updatable = false,
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String email;
+
+    @Column(
+            name = "password",
+            columnDefinition = "TEXT"
+    )
+    private String password;
+
+    @Column(
+            name = "phone_number",
+            columnDefinition = "TEXT"
+    )
+    private String phoneNumber;
+
+    @Column(
+            name = "disable_reason",
+            columnDefinition = "TEXT"
+    )
+    private String disableReason;
+
+    @Column(
+            name = "disabled_at",
+            columnDefinition = "TIMESTAMP WITH TIME ZONE"
+    )
+    private ZonedDateTime disabledAt;
+
+    @CreatedDate
+    @Column(
+            name = "created_at",
+            nullable = false,
+            updatable = false,
+            columnDefinition = "TIMESTAMP WITH TIME ZONE"
+    )
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(
+            name = "updated_at",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITH TIME ZONE"
+    )
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(phoneNumber, user.phoneNumber) && Objects.equals(disableReason, user.disableReason) && Objects.equals(disabledAt, user.disabledAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, phoneNumber, disableReason, disabledAt);
+    }
+}
