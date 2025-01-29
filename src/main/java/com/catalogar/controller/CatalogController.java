30b0ad6dade68f06704b1b7018d66f555cbf0e02
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-01-29 08:32:23 -0300

    Getting started with CatalogController

---
+++ src/main/java/com/catalogar/controller/CatalogController.java
@@ -0,0 +1,23 @@
+package com.catalogar.controller;
+
+import com.catalogar.mapper.CatalogMapper;
+import com.catalogar.service.CatalogService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/catalog")
+public class CatalogController {
+    private final CatalogService catalogService;
+    private final CatalogMapper catalogMapper;
+
+    public CatalogController(
+            CatalogService catalogService,
+            CatalogMapper catalogMapper
+    ) {
+        this.catalogService = catalogService;
+        this.catalogMapper = catalogMapper;
+    }
+
+    //
+}
